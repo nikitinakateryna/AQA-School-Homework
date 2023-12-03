2ea58d9bed8e962b1cd524397858eb453b2976bb
nikitinakateryna <kateryna.nikitina@appflame.com>
2023-12-03 20:43:40 +0200

    add dependency

--- Homework/main (for Homework1)/java/org/example/Homework1.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import com.github.javafaker.Faker;
-
-public class Homework1 {
-    public static void main(String[] args) {
-        System.out.println(Faker.instance().animal().name());
-    }
-}
