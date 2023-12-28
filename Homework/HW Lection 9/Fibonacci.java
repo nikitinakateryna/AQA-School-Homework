ef16b885b16bc123ca39f12a96ae47b89a767c34
nikitinakateryna <kateryna.nikitina@appflame.com>
2023-12-28 17:37:11 +0200

    add fibonacci calculation

---
+++ Homework/HW Lection 9/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.stream.Stream;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        fibCalculate();
+    }
+
+    public static int fibCalculate() {
+        return Stream.iterate(new int[]{0, 1}, i -> new int[]{i[1], i[0] + i[1]})
+                .limit(10)
+                .map(i -> i[0])
+                .reduce((x, y) -> {
+                    System.out.print(x + " ");
+                    return x + y;
+                })
+                .orElse(0);
+
+    }
+}
+
+
