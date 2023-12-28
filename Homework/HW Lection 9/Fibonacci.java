bda6dd8c3f5e2d1bb7e3687b66f5fc00aea62a2a
nikitinakateryna <kateryna.nikitina@appflame.com>
2023-12-28 17:46:55 +0200

    some fixes

--- Homework/HW Lection 9/Fibonacci.java
+++ Homework/HW Lection 9/Fibonacci.java
@@ -1,22 +1,19 @@
-import java.sql.SQLOutput;
 import java.util.stream.Stream;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibCalculate());
+        fibCalculate().forEach(x -> System.out.print(x + " "));
     }
 
-    public static int fibCalculate() {
+    public static Stream<Integer> fibCalculate() {
         return Stream.iterate(new int[]{0, 1}, i -> new int[]{i[1], i[0] + i[1]})
                 .limit(10)
                 .map(i -> i[0])
                 .reduce((x, y) -> {
                     System.out.print(x + " ");
-                    return x + y;
+                    return y;
                 })
-                .orElse(0);
-
+                .stream();
     }
 }
-
 
